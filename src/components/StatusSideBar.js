a6523f12a5ca9eb4e1a4fdafa59d5774990063ac
Tomoaki Iwaya <iwayatomoaki@gmail.com>
2023-02-16 10:48:24 -0300

    Align StatusSideBar Items

--- src/components/StatusSideBar.js
+++ src/components/StatusSideBar.js
@@ -1,25 +1,17 @@
 import {
     Box,
     VStack,
+    HStack,
     StackDivider,
     Heading,
     Tooltip,
     Text,
+    StatNumber,
+    Stat,
 } from "@chakra-ui/react";
 import { QuestionIcon } from '@chakra-ui/icons'
 
 const StatusSideBar = (props) => {
-
-    const toolTipText = () => {
-        return(
-            <Text align='justify'>
-                Indica si los sensores (independientemente del 
-                parámetro que midan) registran o no cantidades en
-                los rangos adecuados para la salud de las personas.
-                Siendo "Bueno" el mejor estado y "Emergencia" el peor.
-            </Text>
-        )
-    }
 
     let good = 0
     let regular = 0
@@ -55,10 +47,21 @@ const StatusSideBar = (props) => {
         }
     }
 
+    const toolTipText = () => {
+        return(
+            <Text align='justify'>
+                Indica si los sensores (independientemente del 
+                parámetro que midan) registran o no cantidades en
+                los rangos adecuados para la salud de las personas.
+                Siendo "Bueno" el mejor estado y "Emergencia" el peor.
+            </Text>
+        )
+    }
+
     return(
         <VStack
             margin='15px'
-            divider={<StackDivider borderColor='gray.500' />}
+            divider={<StackDivider borderColor='gray.500'/>}
             spacing={4}
             align='stretch'>
             <Heading size='md'>
@@ -70,24 +73,78 @@ const StatusSideBar = (props) => {
                         marginBottom='4px'/>
                 </Tooltip>
             </Heading>
-            <Box borderRadius='7px' h='40px' bg='mediumseagreen' p={2}>
-                Bueno : {good}
-            </Box>
-            <Box borderRadius='7px' h='40px' bg='gold' p={2}>
-                Regular : {regular}
-            </Box>
-            <Box borderRadius='7px' h='40px' bg='tomato' p={2}>
-                Alerta : {alert}
-            </Box>
-            <Box borderRadius='7px' h='40px' bg='violet' p={2}>
-                Pre emergencia : {pre}
-            </Box>
-            <Box borderRadius='7px' h='40px' bg='darkorchid' p={2}>
-                Emergencia : {emer}
-            </Box>
-            <Box borderRadius='7px' h='40px' bg='gainsboro' p={2}>
-                No disponible : {nota}
-            </Box>
+            <HStack borderRadius='7px' bg='mediumseagreen'>
+                <Box h='40px' w='80%' p={2}>
+                    Bueno
+                </Box>
+                <Box h='40px' w='20%'>
+                    <Stat>
+                        <StatNumber>
+                            {good}
+                        </StatNumber>
+                    </Stat>
+                </Box>
+            </HStack>
+            <HStack borderRadius='7px' bg='gold'>
+                <Box h='40px' w='80%' p={2}>
+                    Regular
+                </Box>
+                <Box h='40px' w='20%'>
+                    <Stat>
+                        <StatNumber>
+                            {regular}
+                        </StatNumber>
+                    </Stat>
+                </Box>
+            </HStack>
+            <HStack borderRadius='7px' bg='tomato'>
+                <Box h='40px' w='80%' p={2}>
+                    Alerta
+                </Box>
+                <Box h='40px' w='20%'>
+                    <Stat>
+                        <StatNumber>
+                            {alert}
+                        </StatNumber>
+                    </Stat>
+                </Box>
+            </HStack>
+            <HStack borderRadius='7px' bg='violet' >
+                <Box h='40px' w='80%' p={2}>
+                    Pre emergencia
+                </Box>
+                <Box h='40px' w='20%'>
+                    <Stat>
+                        <StatNumber>
+                            {pre}
+                        </StatNumber>
+                    </Stat>
+                </Box>
+            </HStack>
+            <HStack borderRadius='7px' bg='darkorchid'>
+                <Box h='40px' w='80%' p={2}>
+                    Emergencia
+                </Box>
+                <Box h='40px' w='20%'>
+                    <Stat>
+                        <StatNumber>
+                            {emer}
+                        </StatNumber>
+                    </Stat>
+                </Box>
+            </HStack>
+            <HStack borderRadius='7px' bg='gray'>
+                <Box h='40px' w='80%' p={2}>
+                    No disponible
+                </Box>
+                <Box h='40px' w='20%'>
+                    <Stat>
+                        <StatNumber>
+                            {nota}
+                        </StatNumber>
+                    </Stat>
+                </Box>
+            </HStack>
         </VStack>
     )
 }
